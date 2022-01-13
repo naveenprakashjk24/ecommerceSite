a6ec2dfc739bd746e6ba8ec4b799755a2b11eac8
Naveen Prakash <naveenprakashjk24@gmail.com>
2022-01-13 18:51:48 +0530

    template updates

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[
-    os.path.join( BASE_DIR, 'staics')
+    os.path.join( BASE_DIR, 'store/static')
 ]
 
 # Default primary key field type
