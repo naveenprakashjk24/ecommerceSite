9bd745b87aae6049478bd55a282d93b72a08d47b
Naveen Prakash <naveenprakashjk24@gmail.com>
2022-01-21 12:35:31 +0530

    heroku depolyment

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
  
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'store/static/images')
+
+#active django-heroku setting
+django_heroku.settings(locals())
